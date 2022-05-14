f0dd2b72e5a3195b1fdbf59c7fc6e7b6b9a4948a
Артем <timacovartomka@gmail.com>
2022-05-14 09:10:43 +0300

    lesson3 14.05

--- two.py
+++ two.py
@@ -4,7 +4,11 @@ class Monitor:
     rate = 0
     def __init__(self):
         print("Create nem Monitor")
+        self.displayResolution = px
+        self.screenSize = dn
+        self.rate = Hz
 
-lg = Monitor()
+
+lg = Monitor(1500,500,1000)
 philips = Monitor()
 
