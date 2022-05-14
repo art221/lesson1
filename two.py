b72a1ce1b0a9f5774c0c6f4f639d1713a93cdfdb
Артем <timacovartomka@gmail.com>
2022-05-14 09:18:33 +0300

    gg

--- two.py
+++ two.py
@@ -2,13 +2,21 @@ class Monitor:
     displayResolution = 0
     screenSize = 0
     rate = 0
-    def __init__(self):
+    def __init__(self,px,dm,Hz):
         print("Create nem Monitor")
         self.displayResolution = px
-        self.screenSize = dn
+        self.screenSize = dm
         self.rate = Hz
 
 
 lg = Monitor(1500,500,1000)
 philips = Monitor(1250,400,900)
-print(lg.displayResolution)
+print("--==LG==--")
+print("display Resolution:", lg.displayResolution)
+print("screenSize", lg.screenSize)
+print("rate", lg.rate)
+print("--===PHILIPS===--")
+print("--==LG==--")
+print("display Resolution:", philips.displayResolution)
+print("screenSize", philips.screenSize)
+print("rate", philips.rate)
