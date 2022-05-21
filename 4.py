9ebaadf413cf8b6aabf1de900a4141ab45493480
Артем <timacovartomka@gmail.com>
2022-05-21 08:44:26 +0300

    Inheritance of classes

---
+++ 4.py
@@ -0,0 +1,11 @@
+class Bird:
+    color = 0
+    def fly(self):
+        pass
+class Flamingo(Bird):
+    migration = True
+    def standOfOneLeg(self):
+        pass
+class Sparow(Bird):
+    def jumping(self):
+        pass
